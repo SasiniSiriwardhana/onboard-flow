fa0c4ae72dbf0f763b2dbd6717b5f984e9520ebf
Project Developer <sasinisiriwardhana0106@gmail.com>
2026-09-12 22:40:29 +0530

    feat(routers): export clients router in backend routers package

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -1,3 +1,4 @@
 from backend.routers.auth import router as auth_router
+from backend.routers.clients import router as client_router
 
-__all__ = ["auth_router"]
+__all__ = ["auth_router", "client_router"]
